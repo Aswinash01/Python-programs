7f22a76df26973d1accece2f4288727f48773faf
Aswin nair P <aswinnair119@gmail.com>
2026-09-07 12:05:14 +0530

    first commit

---
+++ Python/CO2_1.py
@@ -0,0 +1,7 @@
+#Find the factorial
+
+num=int(input("enter a number "))
+fact=1
+for i in range(1,num+1):
+    fact=fact*i
+print("factorial=",fact)
---
+++ Python/CO2_4.py
@@ -0,0 +1,2 @@
+#Generate a list 4 digit number in a given range with all their even and the  number is a perfect squre
+
